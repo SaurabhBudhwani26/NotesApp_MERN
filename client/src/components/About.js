875e93d31ed14678b953f90ff44b089273b89b09
SaurabhBudhwani26 <budhwanisaurabh@gmail.com>
2023-07-24 16:07:22 +0530

    Navbar and Context

---
+++ client/src/components/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { useContext, useEffect } from 'react'
+import noteContext from '../context/notes/noteContext' 
+
+export default function About() {
+  return (
+    <div>
+      This is about
+    </div>
+  )
+}
---
+++ client/src/components/Home.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="container">
+      <h1 className="my-3">Add a note</h1>
+      <form>
+        <div className="mb-3">
+          <label for="exampleInputEmail1" className="form-label">
+            Title
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+          />
+          <div id="emailHelp" className="form-text">
+            Enter title of your notes here
+          </div>
+        </div>
+        <div className="mb-3">
+          <label for="exampleInputPassword1" className="form-label">
+            Description
+          </label>
+          <textarea
+            type="textarea"
+            className="form-control"
+            id="exampleInputPassword1"
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+      <h1 className="my-3">Your notes</h1>
+    </div>
+  );
+}
